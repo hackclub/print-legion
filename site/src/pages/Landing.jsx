e770f8e1be57e2b8f7dcc49d5680cd73e4e63114
Alex Ren <41168529+qcoral@users.noreply.github.com>
2025-11-21 13:45:00 -0500

    moved to mdx instead of react-markdown

--- site/src/pages/Landing.jsx
+++ site/src/pages/Landing.jsx
@@ -1,40 +1,4 @@
-import ReactMarkdown from 'react-markdown';
-
-const markdownContent = `
-### **For requesters**
-
-1. Post your request in \#printing-legion\! Include your country, the .STL files to print, and a picture of what you need  
-2. Go to [printlegion.hackclub.com](http://printlegion.hackclub.com) and reach out to someone\! Ask them to print your stuff  
-3. Pay for the shipping label using your YSWS card grant. You do not get extra money for this\!  
-4. Repeat
-
-### **For printers:**
-
-1. Sign up by filling out [forms.hackclub.com/printing-legion](http://forms.hackclub.com/printing-legion)  
-2. Wait for someone to reach out\! If you want a job sooner, feel free to grab any you see in \#printing-legion  
-3. Print their part\! Once you do, ping them in \#printing-legion with a picture of their print\!  
-4. Ship it \- Ask for the requester’s info and use their HCB card grant to pay for the label  
-5. Fill out [https://forms.hackclub.com/sent-print](https://forms.hackclub.com/sent-print)  
-6. Once you’ve printed 750g of filament & shipped it all out, you’ll get an email asking for your info\!  
-7. Repeat\!
-
-### Notes:
-
-**Filament reimbursement:**  
-Printing legion is sponsored by Polymaker\! They’ve provided us free filament for the following countries:
-
-- United States  
-- Canada  
-- Australia  
-- Poland  
-- France  
-- Germany  
-- South Africa
-
-Filament will be shipped directly from Polymaker warehouses. These countries are confirmed to not need to pay any customs, so you shouldn’t have issues\!
-
-If you’re not from one of these countries, we’ll send you a $20-25 HCB Card grant to get filament instead!
-`;
+import LandingContent from '../markdown/LandingContent.mdx';
 
 export default function Landing() {
     return (
@@ -44,24 +8,22 @@ export default function Landing() {
             Welcome to the printing legion! This is the international network of 3D printers from Hack Club!
         </p>
         
-        {}
+        {/* Instructions */}
         <div className="mb-6 rounded-lg">
-            <ReactMarkdown
+            <LandingContent
                 components={{
-                    h1: ({node, ...props}) => <h1 className="text-3xl font-bold mb-4 mt-2" {...props} />,
-                    h2: ({node, ...props}) => <h2 className="text-2xl font-bold mb-3 mt-2" {...props} />,
-                    h3: ({node, ...props}) => <h3 className="text-xl font-bold mb-2 mt-2" {...props} />,
-                    p: ({node, ...props}) => <p className="text-base mb-3" {...props} />,
-                    ul: ({node, ...props}) => <ul className="list-disc list-inside mb-3 space-y-1" {...props} />,
-                    ol: ({node, ...props}) => <ol className="list-decimal list-inside mb-3 space-y-1" {...props} />,
-                    li: ({node, ...props}) => <li className="text-base" {...props} />,
-                    strong: ({node, ...props}) => <strong className="font-bold" {...props} />,
-                    em: ({node, ...props}) => <em className="italic" {...props} />,
-                    a: ({node, ...props}) => <a className="text-blue-600 hover:underline" {...props} />,
+                    h1: (props) => <h1 className="text-3xl font-bold mb-4 mt-2" {...props} />,
+                    h2: (props) => <h2 className="text-2xl font-bold mb-3 mt-2" {...props} />,
+                    h3: (props) => <h3 className="text-xl font-bold mb-2 mt-2" {...props} />,
+                    p: (props) => <p className="text-base mb-3" {...props} />,
+                    ul: (props) => <ul className="list-disc list-inside mb-3 space-y-1" {...props} />,
+                    ol: (props) => <ol className="list-decimal list-inside mb-3 space-y-1" {...props} />,
+                    li: (props) => <li className="text-base" {...props} />,
+                    strong: (props) => <strong className="font-bold" {...props} />,
+                    em: (props) => <em className="italic" {...props} />,
+                    a: (props) => <a className="text-blue-600 hover:underline" {...props} />,
                 }}
-            >
-                {markdownContent}
-            </ReactMarkdown>
+            />
         </div>
 
         <div className="my-6 justify-center flex">
@@ -69,4 +31,4 @@ export default function Landing() {
         </div>
         </div>
     );
-    }
+}
